90b04878c91435f6cafd1aa15945ae15a6dc2d3a
leite-1309 <leite.lucas1309@gmail.com>
2026-05-11 10:57:12 -0300

    criando menu

---
+++ cp3.py
@@ -0,0 +1,3 @@
+while True:
+    print("1 - Exibir todos os alunos \n2 - Calcular média geral \n3 - Exibir maior e menor nota \n4 - Exibir aprovados e reprovados \n5 - Buscar aluno por nota \n6 - Buscar aluno por curso \n7 - Gerar relatório JSON \n8 - Exportar relatório TXT \n0 - Encerrar sistema")
+    op = int(input("Escolha uma opção: "))
