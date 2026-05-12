a5c54aa1049c1665910c6eb6722feb16b98fa570
leite-1309 <leite.lucas1309@gmail.com>
2026-05-12 15:05:18 -0300

    finalizando as funcionalidades

--- cp3.py
+++ cp3.py
@@ -2,11 +2,223 @@
 import json
 
 
+def gerar_relatorio_txt():
+    soma = 0
+    total = 0
+    maior_nota = -1
+    menor_nota = 11
+    aluno_maior_nota = ""
+    aluno_menor_nota = ""
+
+    with open("entrada_alunos.txt", "r", encoding="utf-8") as arquivo_txt:
+        for linha in arquivo_txt:
+            linha = linha.strip()
+
+            partes = linha.split("-")
+            nome = partes[1].strip()
+
+            pos_nota = linha.split(":")
+            nota = float(pos_nota[1])
+
+            soma += nota
+            total += 1
+
+            if nota > maior_nota:
+                maior_nota = nota
+                aluno_maior_nota = nome
+
+            if nota < menor_nota:
+                menor_nota = nota
+                aluno_menor_nota = nome
+
+    with open("entrada_alunos.json", "r", encoding="utf-8") as arquivo_json:
+        alunos = json.load(arquivo_json)
+
+        for aluno in alunos:
+            nome = aluno["nome"]
+            nota = aluno["nota"]
+
+            soma += nota
+            total += 1
+
+            if nota > maior_nota:
+                maior_nota = nota
+                aluno_maior_nota = nome
+
+            if nota < menor_nota:
+                menor_nota = nota
+                aluno_menor_nota = nome
+
+    media = soma / total
+
+    with open("relatorio.txt", "w", encoding="utf-8") as arquivo_relatorio:
+        arquivo_relatorio.write("===== RELATÓRIO =====\n\n")
+        arquivo_relatorio.write(f"Total de alunos: {total}\n")
+        arquivo_relatorio.write(f"Média geral: {media:.2f}\n\n")
+        arquivo_relatorio.write("Maior nota:\n")
+        arquivo_relatorio.write(f"{aluno_maior_nota} - {maior_nota}\n\n")
+        arquivo_relatorio.write("Menor nota:\n")
+        arquivo_relatorio.write(f"{aluno_menor_nota} - {menor_nota}\n")
+
+    print("Relatório TXT gerado com sucesso!")
+
+
+def gerar_relatorio_json():
+    soma = 0
+    total = 0
+    aprovados = []
+    reprovados = []
+
+    with open("entrada_alunos.txt", "r", encoding="utf-8") as arquivo_txt:
+        for linha in arquivo_txt:
+            linha = linha.strip()
+
+            partes = linha.split("-")
+            id = partes[0].strip()
+            nome = partes[1].strip()
+            curso = partes[2].strip()
+
+            pos_nota = linha.split(":")
+            nota = float(pos_nota[1])
+
+            aluno = {
+                "id": id,
+                "nome": nome,
+                "curso": curso,
+                "nota": nota
+            }
+
+            soma += nota
+            total += 1
+
+            if nota >= 6:
+                aprovados.append(aluno)
+            else:
+                reprovados.append(aluno)
+
+    with open("entrada_alunos.json", "r", encoding="utf-8") as arquivo_json:
+        alunos = json.load(arquivo_json)
+
+        for aluno in alunos:
+            nota = aluno["nota"]
+
+            aluno_formatado = {
+                "id": f"{aluno['id']:03d}",
+                "nome": aluno["nome"],
+                "curso": aluno["curso"],
+                "nota": nota
+            }
+
+            soma += nota
+            total += 1
+
+            if nota >= 6:
+                aprovados.append(aluno_formatado)
+            else:
+                reprovados.append(aluno_formatado)
+
+    media = soma / total
+
+    relatorio = {
+        "media": media,
+        "total": total,
+        "aprovados": aprovados,
+        "reprovados": reprovados
+    }
+
+    with open("relatorio.json", "w", encoding="utf-8") as arquivo_relatorio:
+        json.dump(relatorio, arquivo_relatorio, indent=4, ensure_ascii=False)
+
+    print("Relatório JSON gerado com sucesso!")
+
+
+
+
+
+
+
+def buscar_curso():
+    encontrados = []
+
+    busca_curso = input("Qual curso deseja buscar? ").strip().upper()
+
+    #Abrindo o arquivo .txt em modo leitura, usando encoding utf-8 para aceitar acentos
+    with open ("entrada_alunos.txt", "r", encoding="utf-8") as arquivo_txt:
+        #Percorrendo o arquivo .txt linha por linha
+        for linha in arquivo_txt:
+            linha = linha.strip()
+            partes = linha.split("-")
+            curso = partes[2].strip().upper()
+            id = partes[0].strip()
+            nome = partes[1].strip()
+            if (curso == busca_curso):
+                encontrados.append(f"{id} - {nome} - {curso}")
+
+    #Abrindo o arquivo .json em modo leitura, usando encoding utf-8 para aceitar acentos
+    with open ("entrada_alunos.json", "r", encoding="utf-8") as arquivo_json:
+        #Lendo o conteudo do json e transformando em uma lista de dicionarios do Python
+        alunos = json.load(arquivo_json)
+        #Percorrendo cada aluno dentro da lista alunos
+        for aluno in alunos:
+            if (aluno["curso"].upper() == busca_curso):
+                encontrados.append(f"{aluno['id']:03d} - {aluno['nome']} - {aluno['curso'].upper()}")
+
+    if not encontrados:
+        print("Não há nenhum aluno desse curso")
+    else:
+        print(f"Alunos de {busca_curso} encontrados: ")
+        for aluno in encontrados:
+            print(aluno)
+    
+
+
+
+
+
+def buscar_nota():
+    encontrados = [] 
+
+    busca_nota = float(input("Qual nota deseja buscar? ").strip())
+
+    if (busca_nota < 0 or busca_nota > 10):
+        print("Valor inválido! As notas só vão de 0 a 10")
+        return
+    
+    #Abrindo o arquivo .txt em modo leitura, usando encoding utf-8 para aceitar acentos
+    with open ("entrada_alunos.txt", "r", encoding="utf-8") as arquivo_txt:
+        #Percorrendo o arquivo .txt linha por linha
+        for linha in arquivo_txt:
+            linha = linha.strip()
+            pos_nota = linha.split(":")
+            nota = float(pos_nota[1])
+            if (nota == busca_nota):
+                partes = linha.split("-")
+                id = partes[0].strip()
+                nome = partes[1].strip()
+                encontrados.append(f"{id} - {nome}")
+
+    #Abrindo o arquivo .json em modo leitura, usando encoding utf-8 para aceitar acentos
+    with open ("entrada_alunos.json", "r", encoding="utf-8") as arquivo_json:
+        #Lendo o conteudo do json e transformando em uma lista de dicionarios do Python
+        alunos = json.load(arquivo_json)
+        #Percorrendo cada aluno dentro da lista alunos
+        for aluno in alunos:
+            if (aluno["nota"] == busca_nota):
+                encontrados.append(f"{aluno['id']:03d} - {aluno['nome']}")
+
+    if (len(encontrados) != 0):            
+        print("Foram encontrados", len(encontrados), "aluno(s) com essa nota.")
+        print("São eles:")
+        for aluno in encontrados:
+            print(aluno)
+    else:
+        print("Nenhum aluno foi encontrado com essa nota")
+
+
+
 #Criando listas vazias para armazenar o nome dos alunos aprovados e reprovados
 aprovados = []
 reprovados = []
-
-
 #Criando a funcao que separa os alunos aprovados e reprovados dos arquivos .txt e .json
 def exibir_aprovados_reprovados():
     #Abrindo o arquivo .txt em modo leitura, usando encoding utf-8 para aceitar acentos
@@ -199,9 +411,50 @@ while True:
             exibir_aprovados_reprovados()
             #Mostrando outra linha para fechar a exibicao das listas
             print("=======================================================================================================================================================\n")
+
+        case 5:
+            #Mostrando uma linha para separar visualmente o resultado do menu
+            print("\n=========================================================")
+            #Chamando a funcao responsavel por buscar os alunos pela nota
+            buscar_nota()
+            #Mostrando outra linha para fechar a exibicao da maior e menor nota
+            print("=========================================================\n")
+
+        case 6:
+            #Mostrando uma linha para separar visualmente o resultado do menu
+            print("\n=========================================================")
+            #Chamando a funcao responsavel por buscar os alunos pelo curso
+            buscar_curso()
+            #Mostrando outra linha para fechar a exibicao da maior e menor nota
+            print("=========================================================\n")   
+
+        case 7:
+            #Mostrando uma linha para separar visualmente o resultado do menu
+            print("\n=========================================================")
+            #Chamando a funcao responsavel por buscar os alunos pelo curso
+            gerar_relatorio_json()
+            #Mostrando outra linha para fechar a exibicao da maior e menor nota
+            print("=========================================================\n")
+
+        case 8:
+            #Mostrando uma linha para separar visualmente o resultado do menu
+            print("\n=========================================================")
+            #Chamando a funcao responsavel por buscar os alunos pelo curso
+            gerar_relatorio_txt()
+            #Mostrando outra linha para fechar a exibicao da maior e menor nota
+            print("=========================================================\n")
+
         #Caso o usuario escolha a opcao 0, o programa encerra o sistema
         case 0:
             #Mensagem avisando que o sistema esta sendo encerrado
             print("Encerrando...")
             #Interrompendo o while True e finalizando o programa
             break
+
+        case _:
+            #Mostrando uma linha para separar visualmente o resultado do menu
+            print("\n=========================================================")
+            print("Opção inválida!")
+            #Mostrando outra linha para fechar a exibicao da maior e menor nota
+            print("=========================================================\n")
+
